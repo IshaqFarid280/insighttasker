28c0310fcf5794b5edc714dd8b2d11eff94accaa
hrandriamanantena <rn_hermann@yahoo.fr>
2024-03-08 14:49:01 +0300

    quickfix for translations

--- lib/languageProvider.dart
+++ lib/languageProvider.dart
@@ -1,9 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider extends ChangeNotifier {
-  String _currentLanguage = 'en';
+  late SharedPreferences _preferences;
+  late Locale _locale;
+  Map<String, String>? _localizedStrings;
 
-  String get currentLanguage => _currentLanguage;
+  LanguageProvider() {
+    _initPreferences();
+  }
+
+  Locale get locale => _locale;
+  set locale(Locale value) {
+    if (_locale != value) {
+      _locale = value;
+      SharedPreferences.getInstance().then((prefs) {
+        prefs.setString('language_code', value.languageCode);
+      }).then((value) {
+        _loadTranslations();
+      }).then((value) {
+        notifyListeners();
+      });
+    }
+  }
+
+  Future<void> _initPreferences() async {
+    _preferences = await SharedPreferences.getInstance();
+    String? savedLocale = _preferences.getString('language_code');
+    if (savedLocale != null) {
+      _locale = Locale(savedLocale);
+    }
+    _loadTranslations();
+    notifyListeners();
+  }
 
   // Define a callback function
   VoidCallback? _onLanguageChangeCallback;
@@ -13,14 +45,26 @@ class LanguageProvider extends ChangeNotifier {
     _onLanguageChangeCallback = callback;
   }
 
-
   void changeLanguage(String newLanguage) {
     print('print the changelnaguge from language provider: $newLanguage');
-    _currentLanguage = newLanguage;
+    locale = Locale(newLanguage);
     notifyListeners(); // Notify listeners of the change
-
 
     // Call the callback function when the language changes
     _onLanguageChangeCallback?.call();
+  }
+
+  String translate(String key) {
+    return _localizedStrings?[key] ?? key;
+  }
+
+  Future<void> _loadTranslations() async {
+    String jsonString =
+        await rootBundle.loadString('assets/i18n/${_locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    _localizedStrings =
+        jsonMap.map((key, value) => MapEntry(key, value.toString()));
+    print(
+        "rebuild in ===> ${_locale.languageCode}; _localizedStrings=$_localizedStrings");
   }
 }
